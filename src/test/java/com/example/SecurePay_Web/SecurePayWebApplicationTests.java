4696e0fd5b605491a239d8b6a99d5cfdeb9a6646
Abduallah Ragab <abduallah.mhr@gmail.com>
2025-07-27 13:19:37 +0300

    initial commit

---
+++ src/test/java/com/example/SecurePay_Web/SecurePayWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecurePay_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurePayWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
